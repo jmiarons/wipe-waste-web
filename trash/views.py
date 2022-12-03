bcb21c4fac39910a6ed208061c499009411b1e48
Arnau Casas <33383702+Casassarnau@users.noreply.github.com>
2022-12-04 00:23:19 +0100

    Added more

--- trash/views.py
+++ trash/views.py
@@ -1,3 +1,5 @@
+import base64
+
 from django.core.cache import cache
 from django.http import HttpResponse, Http404
 from django.shortcuts import redirect
@@ -5,7 +7,7 @@ from django.urls import reverse
 from django.views import View
 from django.views.generic import TemplateView
 
-from trash.models import WrongAnswer, TrashUsed, TrashType
+from trash.models import WrongAnswer, TrashUsed, TrashType, User
 from trash.services.disco import DiscoService
 
 
@@ -16,10 +18,12 @@ class ScanTrash(TemplateView):
         image = request.FILES.get('image', None)
         context = self.get_context_data()
         if image is not None:
+            image_bytes = image.read()
             service = DiscoService()
-            tag = service.get_tag_from_image(image)
-            trash_type = TrashType.objects.get(trashtag_set__name=tag)
-            context.update({'trash_type': trash_type, 'image': image, 'possible_tags': service.get_all_tags()})
+            tag = service.get_tag_from_image(image_bytes)
+            image_base64 = base64.b64encode(image_bytes).decode('utf-8')
+            trash_type = TrashType.objects.get(trashtag__name=tag)
+            context.update({'trash_type': trash_type, 'image': image_base64, 'possible_tags': service.get_all_tags()})
         else:
             context.update({'error': 'Image is required'})
         cache.set('success_attempts', cache.get('success_attempts', 0) + 1)
@@ -37,7 +41,7 @@ class AddRetrainQueue(View):
         if image is not None and tag is not None and tag in DiscoService().get_all_tags():
             WrongAnswer(image=str(image), tag=tag).save()
             self.update_attempts()
-            trash_type = TrashType.objects.get(trashtag_set__name=tag)
+            trash_type = TrashType.objects.get(trashtag__name=tag)
             return redirect(reverse('scan_qr') + '?tag=%s' % trash_type.get_uuid())
         return HttpResponse(status=400)
 
@@ -53,16 +57,30 @@ class ScanTrashContainer(TemplateView):
         context.update({'tag': tag})
         return context
 
+    def get_user(self):
+        id_card = self.request.POST.get('id_card', None)
+        serial_number = self.request.POST.get('serial_number', None)
+        try:
+            if id_card is not None:
+                return User.objects.get(id_card=id_card)
+            if serial_number is not None:
+                return User.objects.get(serial_number=serial_number)
+        except User.DoesNotExist:
+            pass
+        return None
+
     def post(self, request, *args, **kwargs):
-        id_card = request.POST.get('id_card', None)
+        user = self.get_user()
         qr_code = request.POST.get('qr_code', None)
         context = self.get_context_data()
-        if id_card is not None and qr_code is not None:
-            error_text = TrashUsed.create_from_qr(qr_code, id_card)
+        if user is not None and qr_code is not None:
+            error_text = TrashUsed.create_from_qr(qr_code, user)
             if error_text:
                 context.update({'error': error_text})
             else:
                 context.update({'success': True})
+        elif user is None:
+            context.update({'error': 'Id card not registered'})
         else:
             context.update({'error': 'The container did not exist'})
         return self.render_to_response(context)
